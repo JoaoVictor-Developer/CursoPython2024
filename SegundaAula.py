b8af81c3c23eef1de40b877effec843391c63763
JoaoVictor <desenvolvedorjoaovictor@gmail.com>
2025-02-25 14:03:24 -0300

    frist variable in text

---
+++ SegundaAula.py
@@ -0,0 +1,8 @@
+nome = "Usuario002"
+idade = 12
+email = "Usuario002@gmail.com"
+login = "12ab45"
+
+#primeiro tipo de textos personalizaveis 
+if email == "Usuario002@gmail.com":
+    print("seu nome: {}, sua idade: {}".format(nome,idade))
