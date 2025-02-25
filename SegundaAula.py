afce203f8df13534a1f55d2274f20373308e538e
JoaoVictor <desenvolvedorjoaovictor@gmail.com>
2025-02-25 14:10:30 -0300

    Seccond Format Text

--- SegundaAula.py
+++ SegundaAula.py
@@ -3,6 +3,12 @@ idade = 12
 email = "Usuario002@gmail.com"
 login = "12ab45"
 
+carro = "miura sport"
+ano = 1979
+cv = 130
 #primeiro tipo de textos personalizaveis
 if email == "Usuario002@gmail.com":
     print("seu nome: {}, sua idade: {}".format(nome,idade))
+#segundo tipo de textos personalizaveis
+if carro == "miura sport":
+    print(f"modelo: {carro},Ano: {ano}, Cavalos: {cv}")
