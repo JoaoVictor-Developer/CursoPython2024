cb2040acb65d58f0a6cf1bf6363a14f39bd27192
JoaoVictor <desenvolvedorjoaovictor@gmail.com>
2025-02-25 14:34:26 -0300

    FIND()

--- SegundaAula.py
+++ SegundaAula.py
@@ -1,12 +1,18 @@
 #Usuario (OBS:trasformar em obj)
 nome = "Usuario002"
 idade = 12
-email = "UsuAriO002@gMail.com"
+email = "LiraDev@gmail.com"
 login = "12ab45"
 #Carro (OBS:trasformar em obj)
 carro = "miura sport"
 ano = 1979
 cv = 130
+#ProcurandoCoisasNoTexto
+if (email.find("@")) == -1:
+#O Find Só Indentifica A Posiçao Do Caracter NAO RETORNA VALORES BOLEANOS!!!
+    print("Isso nao é um email valido")
+else:
+    print("Login Efetuado")
 #primeiro tipo de textos personalizaveis
 if email == "UsuAriO002@gMail.com":
     print("seu nome: {}, sua idade: {}".format(nome,idade))
