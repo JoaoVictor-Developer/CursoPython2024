ccc76e50a65c1c5d1345200b899dc4c93b323324
JoaoVictor <desenvolvedorjoaovictor@gmail.com>
2025-02-25 14:15:37 -0300

    LowerAndUpper-Texts

--- SegundaAula.py
+++ SegundaAula.py
@@ -1,14 +1,21 @@
+#Usuario (OBS:trasformar em obj)
 nome = "Usuario002"
 idade = 12
-email = "Usuario002@gmail.com"
+email = "UsuAriO002@gMail.com"
 login = "12ab45"
-
+#Carro (OBS:trasformar em obj)
 carro = "miura sport"
 ano = 1979
 cv = 130
 #primeiro tipo de textos personalizaveis
-if email == "Usuario002@gmail.com":
+if email == "UsuAriO002@gMail.com":
     print("seu nome: {}, sua idade: {}".format(nome,idade))
 #segundo tipo de textos personalizaveis
 if carro == "miura sport":
     print(f"modelo: {carro},Ano: {ano}, Cavalos: {cv}")
+#upper/maiuscula
+email = email.upper()
+print(email)
+#minuscula/lower
+email = email.lower()
+print(email)
