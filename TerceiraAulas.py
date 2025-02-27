9c8f7a481bba10f2bb56ed640b3f3e1e06ce8665
JoaoVictor <desenvolvedorjoaovictor@gmail.com>
2025-02-27 15:12:21 -0300

    TermineiATerceiraAulaXD

--- TerceiraAulas.py
+++ TerceiraAulas.py
@@ -1,27 +1,27 @@
 #INPUTS
 
-##Usuario = input("Digite seu nome: ")
-##Email = input("Digite seu email: ")
-##Password = int(input("Digite sua senha: "))
-##
-##if Email[Email.find("@"):] == "gmail.com":
-##    print("Seu email e valido")
-##else:
-##    Email = input("ERRO, Digite seu email gmail: ")
+Usuario = input("Digite seu nome: ")
+Email = input("Digite seu email: ")
+Password = int(input("Digite sua senha: "))
+
+if Email[Email.find("@"):] == "gmail.com":
+    print("Seu email e valido")
+else:
+   Email = input("ERRO, Digite seu email gmail: ")
 
 #Arrays-mercado
 
-##custo = 405
-##vendas = [108,193,10,25,30,99,105,203,12]
-##
-##print(f"o total de vendas foram de: {len(vendas)}")
-##print(f"as vendas foi de {sum(vendas)} e o lucro: ",sum(vendas) - custo)
-##
-##print(f"o maior valor obitido foi o: {max(vendas)}")
-##print(f"o menor valor obitido foi o: {min(vendas)}")
-##
-##print(f"essa foi a primeira venda: {vendas[0]}")
-##
+custo = 405
+vendas = [108,193,10,25,30,99,105,203,12]
+
+print(f"o total de vendas foram de: {len(vendas)}")
+print(f"as vendas foi de {sum(vendas)} e o lucro: ",sum(vendas) - custo)
+
+print(f"o maior valor obitido foi o: {max(vendas)}")
+print(f"o menor valor obitido foi o: {min(vendas)}")
+
+print(f"essa foi a primeira venda: {vendas[0]}")
+
 #Arrays-objects
 produtos = ["macbookM1","GalaxS23","PCgamer","FoneHyperex","RedragonA"]
 miura = ["sport","sport","sport","sport","saga"]
