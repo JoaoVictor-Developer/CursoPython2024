b4378951eb4da4d34cff202cd05d9acc3582ab27
JoaoVictor <desenvolvedorjoaovictor@gmail.com>
2025-02-27 14:48:04 -0300

    Arrays-market

--- TerceiraAulas.py
+++ TerceiraAulas.py
@@ -1,10 +1,26 @@
 #INPUTS
-Usuario = input("Digite seu nome: ")
-Email = input("Digite seu email: ")
-Password = int(input("Digite sua senha: "))
 
-if Email[Email.find("@"):] == "gmail.com":
-    print("Seu email e valido")
-else:
-    Email = input("ERRO, Digite seu email gmail: ")
-#LISTAS
+##Usuario = input("Digite seu nome: ")
+##Email = input("Digite seu email: ")
+##Password = int(input("Digite sua senha: "))
+##
+##if Email[Email.find("@"):] == "gmail.com":
+##    print("Seu email e valido")
+##else:
+##    Email = input("ERRO, Digite seu email gmail: ")
+
+#Arrays-mercado
+
+##custo = 405
+##vendas = [108,193,10,25,30,99,105,203,12]
+##
+##print(f"o total de vendas foram de: {len(vendas)}")
+##print(f"as vendas foi de {sum(vendas)} e o lucro: ",sum(vendas) - custo)
+##
+##print(f"o maior valor obitido foi o: {max(vendas)}")
+##print(f"o menor valor obitido foi o: {min(vendas)}")
+##
+##print(f"essa foi a primeira venda: {vendas[0]}")
+##
+#Arrays-objects
+
